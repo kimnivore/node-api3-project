8cb67955ab6c766b5b4a4405d56e31f199d9730b
Kim Nguyen <kimnivore@gmail.com>
2022-02-09 12:57:20 -0800

    completed put

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -11,7 +11,7 @@ const { logger, validateUserId, validateUser, validatePost } = require('../middl
 const router = express.Router();
 
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
   Users.get(req.query)
     .then(users => {
@@ -25,13 +25,15 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.get('/:id', validateUserId, (req, res, next) => {
-  res.status(200).json(req.user);
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.post('/', validateUser, (req, res, next) => {
+  // RETURN THE NEWLY CREATED USER OBJECT
+  // this needs a middleware to check that the request body is valid
+router.post('/', validateUser, (req, res) => {
   Users.insert(req.body)
     .then(user => {
       res.status(201).json(user);
@@ -41,15 +43,23 @@ router.post('/', validateUser, (req, res, next) => {
       res.status(500).json({
         message: "Error adding user"
       })
-    })
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+    })  
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
+// RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating user"
+      })
+    })
 });
 
 router.delete('/:id', (req, res) => {
